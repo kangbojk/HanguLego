0d99f4d1e1683b15e2e41fafbd3ec09a6541c1cc
joseph <kangbo.jk@gmail.com>
2021-08-13 16:31:41 +0800

    Add offset brick, add dep list in useDrag hook

--- src/components/Brick.tsx
+++ src/components/Brick.tsx
@@ -1,41 +1,61 @@
-import React from 'react'
 import { useDrag } from 'react-dnd';
 import { ItemTypes } from './ItemTypes';
+import { CSSProperties } from 'react';
 
 export interface BrickProps {
     hangul: string
-    onPick?: (char: string) => void;
+    left: number
+    top: number
+    from: string
+    id?: string
+}
+
+const style: CSSProperties = {
+    margin: 6,
+    cursor: 'move',
+    position: 'relative',
 }
 
 export default function Brick(props: BrickProps) {
-    const { hangul } = props;
+    const { hangul, top, left, from } = props;
     const fill = 'black'
     const stroke = 'white'
 
+    //  depends on sent variable
     const [{ isDragging }, drag] = useDrag(
         () => ({
             type: ItemTypes.HANGUL,
-            item: { t: hangul },
+            item: { hangul, top, left, from },
             collect: (monitor) => ({ isDragging: !!monitor.isDragging() }),
-        })
+        }), [top, left, from]
     )
 
-    const handlePick = (ev: React.MouseEvent<HTMLDivElement, MouseEvent>) => {
-        if (props.onPick) {
-            props.onPick(hangul)
-        }
+    // add offset for movable brick
+    if (from === "target") {
+        return (
+            <div
+                style={{
+                    ...style,
+                    backgroundColor: fill,
+                    color: stroke,
+                    opacity: isDragging ? 0.5 : 1,
+                    left,
+                    top,
+                }}
+                ref={drag}
+            >
+                {hangul}
+            </div>
+        )
     }
 
     return (
         <div
             style={{
+                ...style,
                 backgroundColor: fill,
                 color: stroke,
-                width: '100%',
-                height: '100%',
                 margin: 6,
-                cursor: 'move',
-                opacity: isDragging ? 0.5 : 1,
             }}
             ref={drag}
         >

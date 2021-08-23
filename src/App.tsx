8bdf5553484826c89a0e91b524dcffbfa63696d0
joseph <kangbo.jk@gmail.com>
2021-08-23 16:59:04 +0800

    Add react-helmet and change favicon

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,24 @@
 import './App.css';
 import HanguLego from './components/HanguLego';
+import { Helmet } from 'react-helmet';
+
 function App() {
 
   return (
     <div className="App">
+      <Helmet>
+        <title>HanguLego</title>
+        <link rel="canonical" href="https://hangulego.carryou.dev/" />
+        <meta name="description" content="Drag colorful bricks to learn Korean words like playing LEGO" />
+        <meta name="keywords" content="Carryou, 帶你飛, Hangul, Hangul Lego, hangulego, carry you" />
+        <meta property="site_name" content="Carryou 帶你飛"></meta>
+        <meta property="og:site_name" content="Carryou 帶你飛"></meta>
+        <meta property="og:title" content="HanguLego"></meta>
+        <meta property="og:description" content="Drag colorful bricks to learn Korean words like playing LEGO"></meta>
+        <meta name="twitter:title" content="HanguLego"></meta>
+        <meta name="twitter:description" content="Drag colorful bricks to learn Korean words like playing LEGO"></meta>
+      </Helmet>
+
       <header className="App-header">
         <HanguLego />
       </header>

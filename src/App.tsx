5c145d7fdaa646c05291ca5331f1a71bc18d8976
joseph <kangbo.jk@gmail.com>
2021-08-23 17:17:41 +0800

    add meta tag

--- src/App.tsx
+++ src/App.tsx
@@ -14,6 +14,8 @@ function App() {
         <meta property="site_name" content="Carryou 帶你飛"></meta>
         <meta property="og:site_name" content="Carryou 帶你飛"></meta>
         <meta property="og:title" content="HanguLego"></meta>
+        <meta property="og:url" content="https://hangulego.carryou.dev/"></meta>
+        <meta property="og:image" content="https://repository-images.githubusercontent.com/393237880/01165450-e5af-4a69-9465-7d83c7c2e00f" />
         <meta property="og:description" content="Drag colorful bricks to learn Korean words like playing LEGO"></meta>
         <meta name="twitter:title" content="HanguLego"></meta>
         <meta name="twitter:description" content="Drag colorful bricks to learn Korean words like playing LEGO"></meta>

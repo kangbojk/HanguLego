a62c0ff66a12647b7c30df8b518facd5673298f4
Joseph Kang <joseph.kang@citizen.com>
2023-08-06 15:31:42 -0500

    change cname

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,41 @@
-import './App.css';
-import HanguLego from './components/HanguLego';
-import { Helmet } from 'react-helmet';
+import "./App.css";
+import HanguLego from "./components/HanguLego";
+import { Helmet } from "react-helmet";
 
 function App() {
-
   return (
     <div className="App">
       <Helmet>
         <title>HanguLego</title>
-        <link rel="canonical" href="https://hangulego.carryou.dev/" />
-        <meta name="description" content="Drag colorful bricks to learn Korean words like playing LEGO" />
-        <meta name="keywords" content="Carryou, 帶你飛, Hangul, Hangul Lego, hangulego, carry you" />
+        <link rel="canonical" href="https://kangbojk.github.io/HanguLego/" />
+        <meta
+          name="description"
+          content="Drag colorful bricks to learn Korean words like playing LEGO"
+        />
+        <meta
+          name="keywords"
+          content="Carryou, 帶你飛, Hangul, Hangul Lego, hangulego, carry you"
+        />
         <meta property="site_name" content="Carryou 帶你飛"></meta>
         <meta property="og:site_name" content="Carryou 帶你飛"></meta>
         <meta property="og:title" content="HanguLego"></meta>
-        <meta property="og:url" content="https://hangulego.carryou.dev/"></meta>
-        <meta property="og:image" content="https://repository-images.githubusercontent.com/393237880/01165450-e5af-4a69-9465-7d83c7c2e00f" />
-        <meta property="og:description" content="Drag colorful bricks to learn Korean words like playing LEGO"></meta>
+        <meta
+          property="og:url"
+          content="https://kangbojk.github.io/HanguLego/"
+        ></meta>
+        <meta
+          property="og:image"
+          content="https://repository-images.githubusercontent.com/393237880/01165450-e5af-4a69-9465-7d83c7c2e00f"
+        />
+        <meta
+          property="og:description"
+          content="Drag colorful bricks to learn Korean words like playing LEGO"
+        ></meta>
         <meta name="twitter:title" content="HanguLego"></meta>
-        <meta name="twitter:description" content="Drag colorful bricks to learn Korean words like playing LEGO"></meta>
+        <meta
+          name="twitter:description"
+          content="Drag colorful bricks to learn Korean words like playing LEGO"
+        ></meta>
       </Helmet>
 
       <header className="App-header">
